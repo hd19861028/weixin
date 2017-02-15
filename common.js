1083d2bdf3369ebe0588e4d3ae64e706fc02f54f
marshu <282642547@qq.com>
2017-02-15 22:16:48 +0800

    1.3.2

--- common.js
+++ common.js
@@ -3,6 +3,7 @@ exports.common = require('./lib/index');
 exports.fileinfo = require('./lib/fileinfo');
 exports.secret = require('./lib/secret');
 exports.weixin = require('./lib/weixin');
+exports.cache = require('./lib/cache');
 exports.request = require('./lib/http-request');
 exports.prototype = require('./lib/prototype');
 exports.agent = require('./lib/agent');
